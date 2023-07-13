212c5188e73250302d95036819c10a9ace885c5c
SunnnySong <tjsghktjswls@naver.com>
2023-07-14 00:42:33 +0900

    chore: TabBarController에서 extension으로 코드 분리
    - TabBarController의 UITabBarControllerDelegate 프로토콜 채택 및 해당 함수 구현을 extension으로 코드를 분리했다.

--- LottoDairy/TabBarScene/TabBarController.swift
+++ LottoDairy/TabBarScene/TabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class TabBarController: UITabBarController, UITabBarControllerDelegate, TabBarFlowProtocol {
+final class TabBarController: UITabBarController, TabBarFlowProtocol {
     
     var onViewWillAppear: ((UINavigationController) -> ())?
     
@@ -22,7 +22,7 @@ final class TabBarController: UITabBarController, UITabBarControllerDelegate, Ta
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        if let controller = viewControllers?.first as? UINavigationController {
+        if let controller = viewControllers?[1] as? UINavigationController {
             onViewWillAppear?(controller)
         }
     }
@@ -30,23 +30,24 @@ final class TabBarController: UITabBarController, UITabBarControllerDelegate, Ta
     func configureViewControllers() {
         self.viewControllers = TabBarComponents.allCases.map { makeTabBarViewControllers($0) }
         selectedIndex = 1
-        
     }
     
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        guard let controller = viewControllers?[selectedIndex] as? UINavigationController else { return }
-        
-        // 현재 문제 : 아래 코드가 실행되지 않아 flow 연결이 되지 않는다.
-        if selectedIndex == 1 {
-            onHomeFlowSelect?(controller)
-        }
-    }
-    
-    // MARK: Functions - Private
     private func makeTabBarViewControllers(_ type: TabBarComponents) -> UINavigationController {
         let viewController = UINavigationController()
         viewController.tabBarItem.title = type.title
         viewController.tabBarItem.image = UIImage(systemName: type.systemName)
         return viewController
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        guard let controller = viewControllers?[selectedIndex] as? UINavigationController else { return }
+        
+        // 현재 문제 : UITabBarControllerDelegate이 작동하지 않는다.
+        if selectedIndex == 1 {
+            onHomeFlowSelect?(controller)
+        }
     }
 }
