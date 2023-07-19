8b6a02ea760ece0b3cf6a49ed67aad3025e80787
seunghyunCheon <csh7215@naver.com>
2023-07-19 15:08:29 +0900

    feat: SignUpValidationState 상수 생성

---
+++ LottoDairy/Domain/UseCase/Account/Protocol/SignUpUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpUseCase.swift
+//  LottoDairy
+//
+//  Created by Brody on 2023/07/19.
+//
+
+import Combine
+
+protocol SignUpUseCase {
+    var nickname: String { get set }
+    var nicknameValidationState: 
+}
