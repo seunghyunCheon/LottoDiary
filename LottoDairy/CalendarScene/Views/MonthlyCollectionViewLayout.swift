0607e3a6495e8da34675b3cd22d9ca36073c9a10
SunnnySong <tjsghktjswls@naver.com>
2023-08-17 03:23:48 +0900

    refactor: MonthlyCollectionViewLayout의 비율 수정

--- LottoDairy/CalendarScene/Views/MonthlyCollectionViewLayout.swift
+++ LottoDairy/CalendarScene/Views/MonthlyCollectionViewLayout.swift
@@ -11,18 +11,15 @@ struct MonthlyCollectionViewLayout {
 
     func createLayout() -> UICollectionViewCompositionalLayout {
         let layout = UICollectionViewCompositionalLayout { (sectionNum, env) -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0 / 7),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-            let itemSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth((1.0 / 7)), heightDimension: .fractionalHeight(1.0))
-            let item: NSCollectionLayoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(1.0 / 6))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
-            let groupSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight((1.0 / 7)))
-            let group: NSCollectionLayoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-
-            let groupSize2: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight((1.0)))
-            let group2: NSCollectionLayoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize2, subitems: [group])
-
-            let section: NSCollectionLayoutSection = NSCollectionLayoutSection(group: group2)
+            let section = NSCollectionLayoutSection(group: group)
             return section
         }
         return layout
