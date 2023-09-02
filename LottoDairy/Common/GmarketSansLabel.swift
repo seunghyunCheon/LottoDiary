6fe11fa7aff6f4d411cfd9bf284c1c1add23d3b8
SunnnySong <tjsghktjswls@naver.com>
2023-09-03 01:55:08 +0900

    feat: GmarketSansLabel에 attributedText 설정하는 init 추가

--- LottoDairy/Common/GmarketSansLabel.swift
+++ LottoDairy/Common/GmarketSansLabel.swift
@@ -23,6 +23,20 @@ class GmarketSansLabel: UILabel {
         self.textAlignment = alignment
     }
 
+    init(attributedText: NSAttributedString,
+         color: UIColor = .white,
+         alignment: NSTextAlignment = .center,
+         size: UIFont.Size,
+         weight: UIFont.CustomWeight) {
+        super.init(frame: .zero)
+
+        self.attributedText = attributedText
+        self.font = .gmarketSans(size: size, weight: weight)
+        self.adjustsFontForContentSizeCategory = true
+        self.textColor = color
+        self.textAlignment = alignment
+    }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
