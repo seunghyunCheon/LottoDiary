e1e4c91c5f0697cd53ca28db5ef69a829361b06e
SunnnySong <tjsghktjswls@naver.com>
2023-09-03 00:49:41 +0900

    feat: UICollectionView에서 register과 dequeue 관련 extensions 추가

---
+++ LottoDairy/Support/Extension/UICollectionViewCell+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+Extensions.swift
+//  LottoDairy
+//
+//  Created by Sunny on 2023/09/03.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+
+    static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
