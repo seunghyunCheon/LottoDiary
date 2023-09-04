dc98e91bacaa7436bb84baa444e6f5670caf3326
SunnnySong <tjsghktjswls@naver.com>
2023-09-04 21:48:51 +0900

    refactor: 상태값이 없는 Usecase로 리팩토링
    - 상태값은 viewModel에 존재해야 함.
    - usecase는 repository를 이용한 데이터 가공을 해야함.

--- LottoDairy/Domain/UseCase/Chart/DefaultChartUseCase.swift
+++ LottoDairy/Domain/UseCase/Chart/DefaultChartUseCase.swift
@@ -10,17 +10,28 @@ import Combine
 
 final class DefaultChartUseCase: ChartUseCase {
 
-    var year: Int = Calendar.current.component(.year, from: Date())
-    var month: Int = Calendar.current.component(.month, from: Date())
+//    private let repository: CoreDataRepository
 
-    var dateHeaderValue = CurrentValueSubject<[Int], Never>([])
+//    init(repository: CoreDataRepository) {
+//        self.repository = repository
+//    }
 
-    func loadDateHeaderValue() {
-        self.dateHeaderValue.send([year, month])
-    }
+    // Repository: CoreData에 저장되어있는 가장 오래된 데이터의 년도 조회
+    // UseCase: Repository의 오래된 년도 조회 함수의 return값 ~ 현재 년도까지 [Int] 배열 반환하는 함수
+    func makeRangeOfYear() -> AnyPublisher<[Int], Never> {
+        return Future<[Int], Never> { promise in
+            // 비동기로 CoreData에서 oldestYear fetch하는 함수 실행
+            //        let oldestYear = repository.fetchOldestYear()
+            let oldestYear = 2021
+            let thisYear = Calendar.current.component(.year, from: Date())
 
-    func setDateHeaderValue(_ date: [Int]) {
-        self.year = date[0]
-        self.month = date[1]
+            var years = [Int]()
+            for year in oldestYear...thisYear {
+                years.append(year)
+            }
+
+            promise(.success(years))
+        }
+        .eraseToAnyPublisher()
     }
 }
--- LottoDairy/Domain/UseCase/Chart/Protocol/ChartUseCase.swift
+++ LottoDairy/Domain/UseCase/Chart/Protocol/ChartUseCase.swift
@@ -8,10 +8,5 @@
 import Combine
 
 protocol ChartUseCase {
-    var year: Int { get set }
-    var month: Int { get set }
-    var dateHeaderValue: CurrentValueSubject<[Int], Never> { get }
-
-    func loadDateHeaderValue()
-    func setDateHeaderValue(_ date: [Int])
+    func makeRangeOfYear() -> AnyPublisher<[Int], Never>
 }
