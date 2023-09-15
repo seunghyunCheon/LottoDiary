9f9a966dab56467064b44fc45f2faa29236409ad
seunghyunCheon <csh7215@naver.com>
2023-09-15 18:49:55 +0900

    feat: AddLottoUseCase 프로토콜 정의.

---
+++ LottoDairy/Domain/UseCase/Calendar/Protocol/AddLottoUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  AddLottoUseCase.swift
+//  LottoDairy
+//
+//  Created by Brody on 2023/09/15.
+//
+
+import Combine
+
+protocol AddLottoUseCase {
+    var lottoType: LottoType { get set }
+    var purchaseAmount: CurrentValueSubject<Int?, Never> { get set }
+    var winningAmount: CurrentValueSubject<Int?, Never> { get set }
+    func setLottoType(_ type: String)
+    func setPurchaseAmount(_ text: String)
+    func setWinningAmount(_ text: String)
+    func addLotto() -> Lotto
+}
