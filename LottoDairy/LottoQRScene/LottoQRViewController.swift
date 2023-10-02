e697172da718a2bb6fe64a278b287a033ec7515c
SunnnySong <tjsghktjswls@naver.com>
2023-10-03 06:32:17 +0900

    feat: LottoQRViewController에 qrReaderView 넣기

--- LottoDairy/LottoQRScene/LottoQRViewController.swift
+++ LottoDairy/LottoQRScene/LottoQRViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 final class LottoQRViewController: UIViewController, LottoQRFlowProtocol {
 
+    private lazy var qrReaderView: UIView = {
+        let qrReaderView = QRReaderView(frame: view.bounds)
+        qrReaderView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return qrReaderView
+    }()
+
+    init() {
+        print("로또 컨트롤러 생성 완료")
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.addSubview(qrReaderView)
     }
-
 }
