d591450b067f4592f63bc71d5a17b4e95c95d1f4
SunnnySong <tjsghktjswls@naver.com>
2023-10-11 15:15:57 +0900

    chore: 주석 삭제

--- LottoDairy/Coordinator/Protocols/Coordinator.swift
+++ LottoDairy/Coordinator/Protocols/Coordinator.swift
@@ -5,9 +5,6 @@
 //  Created by Sunny on 2023/06/27.
 //
 
-// Domain, Data, Presentation, Protocol, Application, Resourece
-// 
-
 protocol Coordinator: AnyObject {
     
     var childCoordinators: [Coordinator] { get }
