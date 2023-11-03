b5cd311813c13de9ee4c43ef804076081cfb4534
SunnnySong <tjsghktjswls@naver.com>
2023-11-03 14:51:03 +0900

    feat: LottoValidationUseCase 생성 및 함수 추가

--- LottoDairy/Domain/UseCase/LottoValidation/Protocol/LottoValidationUseCase.swift
+++ LottoDairy/Domain/UseCase/LottoValidation/Protocol/LottoValidationUseCase.swift
@@ -5,6 +5,10 @@
 //  Created by Sunny on 11/2/23.
 //
 
-protocol LottoValidationUseCase {
+import Combine
 
+protocol LottoValidationUseCase {
+    func fetchLottosWithoutWinningAmount() -> AnyPublisher<[Lotto], Error>
+    func fetchLottoResult(_ roundNumber: Int) -> AnyPublisher<[[Int]]?, Error>
+    func updateWinningAmount(lotto: Lotto, amount: Int)
 }
