436dbf20b68f2eaccf47e698d0b488634268512c
SunnnySong <tjsghktjswls@naver.com>
2023-11-03 14:54:31 +0900

    feat: compareLottoNumbers 함수 구현
    - 구매한 로또 숫자들과 결과를 비교하는 함수
    - 각 배열 당 로또 결과 숫자와 동일한 숫자 count를 반환
    - 2등/3등의 경우 보너스 숫자로 갈리기 때문에 숫자 count를 5/-5로 설정

--- LottoDairy/LottoValidationFlow/LottoValidationController.swift
+++ LottoDairy/LottoValidationFlow/LottoValidationController.swift
@@ -19,4 +19,18 @@ final class LottoValidationController {
     init(lottoValidationUseCase: LottoValidationUseCase) {
         self.lottoValidationUseCase = lottoValidationUseCase
     }
+
+    private func compareLottoNumbers(_ numbers: [[Int]], with result: [Int], bonus: [Int]) -> [Int?] {
+        // 로또 번호와 결과 비교 로직 구현
+        let comparedNumbers = numbers.map { number in
+            let count = Set(number).intersection(Set(result)).count
+            if count == 5 && number.contains(bonus[0]) {
+                // 번호 5개 + 보너스 번호까지 있으면 "-5" (2등)
+                return Optional(-5)
+            } else {
+                return count > 2 ? count : nil
+            }
+        }
+        return comparedNumbers
+    }
 }
