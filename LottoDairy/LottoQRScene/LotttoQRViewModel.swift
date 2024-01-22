e1d8b3cd44820c285aaa332a50611df40b4ff648
SunnnySong <tjsghktjswls@naver.com>
2024-01-23 01:23:18 +0900

    feat: LottoQRViewModel - VC 연결 로직 구현
    - 유효성 검사 send
    - 유효성 검사 통과한 url을 크롤링 함수 연결한 결과 send

--- LottoDairy/LottoQRScene/LotttoQRViewModel.swift
+++ LottoDairy/LottoQRScene/LotttoQRViewModel.swift
@@ -10,8 +10,7 @@ import Combine
 
 enum LottoQRState {
     case invalid
-    case notAnnounced
-    case valid
+    case valid(String)
 }
 
 final class LottoQRViewModel {
@@ -24,10 +23,13 @@ final class LottoQRViewModel {
     
     struct Output {
         let lottoQRValidation = PassthroughSubject<LottoQRState, Never>()
+        // 로또 성공적 조회 이후 back 했을 때, 달력 화면으로 전환하기
     }
     
     private var lottoURL = CurrentValueSubject<String, Never>("")
-    
+
+    private var validation = CurrentValueSubject<LottoQRState, Never>(.invalid)
+
     private var cancellables: Set<AnyCancellable> = []
     
     init(lottoQRUseCase: LottoQRUseCase) {
@@ -41,22 +43,54 @@ final class LottoQRViewModel {
     
     private func configureInput(_ input: Input) {
         input.qrCodeDidRecognize
-            .flatMap { url -> AnyPublisher<Lotto, Error> in
-                return self.lottoQRUseCase.crawlLottoResult(url)
-            }
             .sink { completion in
-                
-            } receiveValue: { lotto in
-                
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { url in
+                // url 유효성 검사
+                if self.lottoQRUseCase.valid(url) {
+                    #if DEBUG
+                    print("ℹ️ 로또 유효성 검사 통과")
+                    print("-----------------------------------------")
+                    #endif
+
+                    // 유효하다면 lottoURL에 send
+                    self.validation.send(.valid(url))
+                    self.lottoURL.send(url)
+                } else {
+                    #if DEBUG
+                    print("ℹ️ 로또 유효성 검사 X")
+                    print("-----------------------------------------")
+                    #endif
+
+                    // 유효하지 않다면
+                    self.validation.send(.invalid)
+                }
             }
             .store(in: &cancellables)
-        
     }
     
     private func configureOutput(from input: Input) -> Output {
         let output = Output()
-        
+
+        self.validation
+            .sink { state in
+                output.lottoQRValidation.send(state)
+            }
+            .store(in: &cancellables)
+
+        self.lottoURL
+            .flatMap { url -> AnyPublisher<Lotto, Error> in
+                return self.lottoQRUseCase.crawlLottoResult(url)
+            }
+            .sink(receiveCompletion: { completion in
+                print(completion)
+            }, receiveValue: { lotto in
+                print(lotto)
+            })
+            .store(in: &cancellables)
+
         return output
     }
-    
 }
