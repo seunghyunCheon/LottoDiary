517dac2ca34bbbfc7e7465d332faf2fae580ea47
SunnnySong <tjsghktjswls@naver.com>
2024-01-23 13:58:59 +0900

    feat: URLResponse valid 관련 extension 추가

---
+++ LottoDairy/Support/Extension/URLResponse+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  URLResponse+Extensions.swift
+//  LottoDairy
+//
+//  Created by Sunny on 1/23/24.
+//
+
+import Foundation
+
+extension URLResponse {
+    private var successRange: ClosedRange<Int> {
+        return 200...299
+    }
+
+    var checkResponse: Bool {
+        guard let httpResponse = self as? HTTPURLResponse, successRange.contains(httpResponse.statusCode) else {
+            return false
+        }
+
+        return true
+    }
+}
