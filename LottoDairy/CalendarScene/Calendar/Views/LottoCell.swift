7e32d32072fe3c13059b053d40aa5f651c62a462
SunnnySong <tjsghktjswls@naver.com>
2024-03-08 22:02:49 +0900

    fix: LottoCell의 디자인 속성 Private 변경 및 가독성을 위한 layout 코드 extension 분리

--- LottoDairy/CalendarScene/Calendar/Views/LottoCell.swift
+++ LottoDairy/CalendarScene/Calendar/Views/LottoCell.swift
@@ -9,26 +9,26 @@ import UIKit
 
 final class LottoCell: UICollectionViewCell {
     
-    var lottoImageView: UIImageView = {
+    private var lottoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    var titleLabel: UILabel = {
+    private var titleLabel: UILabel = {
         let label = GmarketSansLabel(text: "로또", size: .body, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    var purchaseLabel: UILabel = {
+    private var purchaseLabel: UILabel = {
         let label = GmarketSansLabel(text: "구매 금액", size: .middleCaption, weight: .medium)
         label.setContentHuggingPriority(.required, for: .horizontal)
         label.textAlignment = .center
         return label
     }()
     
-    var purchaseAmountLabel: UILabel = {
+    private var purchaseAmountLabel: UILabel = {
         let label = GmarketSansLabel(size: .subheadLine, weight: .bold)
         label.setContentHuggingPriority(.defaultLow, for: .horizontal)
         label.textAlignment = .right
@@ -36,21 +36,21 @@ final class LottoCell: UICollectionViewCell {
         return label
     }()
     
-    var purchaseStackView: UIStackView = {
+    private var purchaseStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .center
         return stackView
     }()
     
-    var winningLabel: UILabel = {
+    private var winningLabel: UILabel = {
         let label = GmarketSansLabel(text: "당첨 금액", size: .middleCaption, weight: .medium)
         label.setContentHuggingPriority(.required, for: .horizontal)
         label.textAlignment = .center
         return label
     }()
     
-    var winningAmountLabel: UILabel = {
+    private var winningAmountLabel: UILabel = {
         let label = GmarketSansLabel(size: .subheadLine, weight: .bold)
         label.setContentHuggingPriority(.defaultLow, for: .horizontal)
         label.textAlignment = .right
@@ -58,13 +58,13 @@ final class LottoCell: UICollectionViewCell {
         return label
     }()
     
-    var winningStackView: UIStackView = {
+    private var winningStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .center
         return stackView
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
@@ -79,6 +79,36 @@ final class LottoCell: UICollectionViewCell {
         self.layer.masksToBounds = true
     }
 
+    override func prepareForReuse() {
+        self.backgroundColor = .clear
+        self.lottoImageView.image = nil
+        self.titleLabel.text = nil
+        self.purchaseAmountLabel.text = nil
+        self.winningAmountLabel.text = nil
+    }
+
+    func configure(with lotto: Lotto) {
+        lottoImageView.image = UIImage(named: lotto.type.imageName)
+        titleLabel.text = lotto.type.rawValue
+        purchaseAmountLabel.text = String(lotto.purchaseAmount.convertToDecimal()) + " 원"
+        if lotto.winningAmount == -1 {
+            winningAmountLabel.text = "미지정"
+        } else {
+            winningAmountLabel.text = String(lotto.winningAmount.convertToDecimal()) + " 원"
+        }
+
+        switch lotto.type {
+        case .lotto:
+            self.backgroundColor = .designSystem(.mainOrange)
+        case .spitto:
+            self.backgroundColor = .designSystem(.mainBlue)
+        }
+    }
+}
+
+// MARK: Layout
+extension LottoCell {
+    
     private func setupLayout() {
         self.addSubview(lottoImageView)
         NSLayoutConstraint.activate([
@@ -87,28 +117,28 @@ final class LottoCell: UICollectionViewCell {
             lottoImageView.widthAnchor.constraint(equalToConstant: 25),
             lottoImageView.heightAnchor.constraint(equalToConstant: 25),
         ])
-        
+
         self.addSubview(titleLabel)
         NSLayoutConstraint.activate([
             titleLabel.leadingAnchor.constraint(equalTo: self.lottoImageView.trailingAnchor, constant: 5),
             titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
-        
-        
+
+
         self.addSubview(self.purchaseStackView)
         self.purchaseStackView.addArrangedSubview(purchaseLabel)
         self.purchaseStackView.addArrangedSubview(purchaseAmountLabel)
         NSLayoutConstraint.activate([
             purchaseStackView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
         ])
-        
+
         self.addSubview(self.winningStackView)
         self.winningStackView.addArrangedSubview(winningLabel)
         self.winningStackView.addArrangedSubview(winningAmountLabel)
         NSLayoutConstraint.activate([
             winningStackView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
         ])
-        
+
         let informationStackView = UIStackView()
         informationStackView.axis = .vertical
         informationStackView.alignment = .center
@@ -124,31 +154,4 @@ final class LottoCell: UICollectionViewCell {
             informationStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15)
         ])
     }
-    
-    func configure(with lotto: Lotto) {
-        lottoImageView.image = UIImage(named: lotto.type.imageName)
-        titleLabel.text = lotto.type.rawValue
-        purchaseAmountLabel.text = String(lotto.purchaseAmount.convertToDecimal()) + " 원"
-        if lotto.winningAmount == -1 {
-            winningAmountLabel.text = "미지정"
-        } else {
-            winningAmountLabel.text = String(lotto.winningAmount.convertToDecimal()) + " 원"
-        }
-        
-        switch lotto.type {
-        case .lotto:
-            self.backgroundColor = .designSystem(.mainOrange)
-        case .spitto:
-            self.backgroundColor = .designSystem(.mainBlue)
-        }
-    }
-    
-    override func prepareForReuse() {
-        self.backgroundColor = .clear
-        self.lottoImageView.image = nil
-        self.titleLabel.text = nil
-        self.purchaseAmountLabel.text = nil
-        self.winningAmountLabel.text = nil
-    }
 }
-
