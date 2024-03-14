39efc518132a86042480a9daa0a68b22bf8f0e66
SunnnySong <tjsghktjswls@naver.com>
2024-03-15 01:31:18 +0900

    feat: 1~46 숫자 중 로또 번호 랜덤 배열로 던저주는 함수 추가

--- LottoDairy/Support/Extension/Int+Extensions.swift
+++ LottoDairy/Support/Extension/Int+Extensions.swift
@@ -21,4 +21,17 @@ extension Int {
 
         return (numberFormatter.string(from: NSNumber(value: self)) ?? "") + "%"
     }
+
+    static let lottoRange: Range<Int> = 1..<46
+
+    static func makeRandomIntArray(count: Int) -> [Int] {
+        var returnArray: [Int] = []
+        for _ in 0..<count {
+            returnArray.append(Int.random(in: lottoRange))
+        }
+
+        let sortedRandomReturnArray = returnArray.sorted()
+
+        return sortedRandomReturnArray
+    }
 }
