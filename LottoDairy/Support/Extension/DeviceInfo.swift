dda18685da3eec8c8b21d6284caa697cb916d604
SunnnySong <tjsghktjswls@naver.com>
2024-03-15 01:31:33 +0900

    feat: DeviceInfo 추가

---
+++ LottoDairy/Support/Extension/DeviceInfo.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.swift
+//  LottoDairy
+//
+//  Created by Sunny on 3/14/24.
+//
+
+import UIKit
+
+struct DeviceInfo {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
